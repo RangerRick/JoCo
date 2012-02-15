5795b8f1445e171ce0bd072a537460ee3750480e
Benjamin Reed <rangerrick@befunk.com>
2012-02-15 11:37:47 -0500

    allow viewing favorite list

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ TOPDIR=`cd $MYDIR; pwd`
 pushd "$TOPDIR" >/dev/null 2>&1
 
 	git pull >update.log 2>&1
+	git submodule init >>update.log 2>&1
+	git submodule update >>update.log 2>&1
 
 	DATE=`date '+%s'`
 	GITHASH=`git log -1 | grep -E '^commit' | cut -d' ' -f2`
